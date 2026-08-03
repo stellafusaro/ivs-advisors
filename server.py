5eb9f08db6e608a9bb5065cf56434ad58728ad9d
Stella Fusaro <scf2151@columbia.edu>
2026-08-03 14:17:01 -0400

    added projects tab

--- server.py
+++ server.py
@@ -10,6 +10,22 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/projects")
+def projects():
+	return render_template("projects.html")
+
+@app.route("consultant")
+def consultant():
+	return render_template("consultant.html")
+
+@app.route("cde")
+ def cde():
+         return render_template("cde.html")
+
+@app.route("banker")
+ def banker():
+         return render_template("banker.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
 
